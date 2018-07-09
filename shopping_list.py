b54b79f4f440384af8d5514fa90596e5666d3112
haptikfeedback <haptik.feedback@gmail.com>
2018-07-08 21:05:22 -0700

    lessons update

--- shopping_list.py
+++ shopping_list.py
@@ -1,36 +1,82 @@
+import os
+
 shopping_list = []
 
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def show_help():
+    clear_screen()
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
 ENTER 'SHOW' to show your list.
+ENTER 'REMOVE' to delete an item from your list.
 Enter 'HELP' for this help.
 """)
 
 
 def add_to_list(item):
-    shopping_list.append(item)
-    print("Added! List has {} items.".format(len(shopping_list)))
+    show_list()
+    if len(shopping_list):
+        position = input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+
+    show_list()
 
 
-# Define a function named show_list that prints all the items in the list
 def show_list():
+    clear_screen()
+
     print("Here's your list:")
+
+    index = 1
     for item in shopping_list:
-        print(item)
+        print("{}. {}".format(index, item))
+        index += 1
+
+    print("-"*10)
+
+
+def remove_from_list():
+    show_list()
+    what_to_remove = input("What would you like to remove?\n> ")
+    try:
+        shopping_list.remove(what_to_remove)
+    except ValueError:
+        pass
+    show_list()
+
 
 show_help()
 while True:
     new_item = input("> ")
 
-    if new_item == 'DONE':
+    if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
         break
-    elif new_item == 'SHOW':
+    elif new_item.upper() == 'SHOW':
         show_list()
         continue
-    elif new_item == 'HELP':
+    elif new_item.upper() == 'HELP':
         show_help()
         continue
+    elif new_item.upper() == 'REMOVE':
+        remove_from_list()
+    else:
+        add_to_list(new_item)
 
-    add_to_list(new_item)
+show_list()
